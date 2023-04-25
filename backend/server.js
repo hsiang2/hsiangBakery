8c390f5142ddec622748f2e99976e9dfca99d2ae
hsiang2 <q224399377@gmail.com>
2023-04-25 14:01:41 +0800

    Test

--- backend/server.js
+++ backend/server.js
@@ -47,6 +47,9 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    //test
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
     // app.use(express.static(path.join(__dirname, '../frontend', 'build')))
     // app.get('/*', (req, res) => {
     //     res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
